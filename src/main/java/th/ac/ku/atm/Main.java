33e3037de90680fdee574056f9efa192b192bdf0
Chutikarn <chutikarn.mo@ku.th>
2021-07-22 12:59:03 +0700

    spring annotation

---
+++ src/main/java/th/ac/ku/atm/Main.java
@@ -0,0 +1,14 @@
+package th.ac.ku.atm;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("bean.xml");
+
+        AtmUI atmUI = context.getBean(AtmUI.class);
+        atmUI.run();
+    }
+}
